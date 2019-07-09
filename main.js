0e28b76bef6685ad3745c0488d8a2b28f16f1426
TYG <793557590@qq.com>
2019-07-10 01:24:03 +0800

    feat: add function statisticsByBarcode

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ function getValidBarcodes(allItems, barcodes){
     });
     return validBarcodes;
 }
+
+function statisticsByBarcode(validBarcodes){
+    let statisticsBarcodes = [];
+    let isContain = false;
+    for(let i = 0; i  < validBarcodes.length; i++){
+        for(let j = 0; j < statisticsBarcodes.length; j++){
+            if(statisticsBarcodes[j]['id'] === validBarcodes[i]){
+                isContain = true;
+                statisticsBarcodes[j]['count'] += 1;
+                break;
+            }
+        }
+        if(!isContain)
+            statisticsBarcodes.push({id:validBarcodes[i],count:1});
+        isContain = false;
+    }
+    return statisticsBarcodes;
+}
