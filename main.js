a4ff5dbbddc0327ee117dd7a97806cadfd75bba3
TYG <793557590@qq.com>
2019-07-10 02:48:04 +0800

    feat: add function createReceipts

--- main.js
+++ main.js
@@ -26,3 +26,26 @@ function statisticsByBarcode(validBarcodes){
     }
     return statisticsBarcodes;
 }
+
+function createReceipts(allItems, statisticsBarcodes){
+    let sum = 0;
+    let str = undefined;
+    let receipts = 'Receipts\n'+
+    '------------------------------------------------------------\n';
+    allItems.forEach(function(item){
+        statisticsBarcodes.forEach(function(statisticsBarcode){
+            if(statisticsBarcode['id'] == item['id']){
+                str = '                                            ';
+                str = item['name'] + str.substring(item['name'].length, (33-item['price'].toString().length))
+                    + item['price']
+                    + str.substring(33, (44 - statisticsBarcode['count'].toString().length)) + statisticsBarcode['count']
+                    + '\n';
+                receipts += str;
+                sum += statisticsBarcode['count']*item['price'];
+            }
+        });
+    });
+    receipts += '------------------------------------------------------------\n'+
+            'Price: '+sum;
+    return receipts;
+}
